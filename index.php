daad5a406fbad279a8dfcd1ca514421fc3dd0f58
Michiel Coulier <michielcoulier@gmail.com>
2020-09-29 15:12:44 +0200

    got image working with search

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ error_reporting(E_ALL);
 
 $pokemonData = file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $_GET['id']);
 $data =  json_decode($pokemonData, true);
+$image = $data['sprites']['front_shiny'];
+$imageData = base64_encode(file_get_contents($image));
 
 echo $data['name'];
-
-echo count($pokemonData);
+echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
+//echo var_dump($data);
