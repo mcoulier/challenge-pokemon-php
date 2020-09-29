cb81b77e64eab2c5cd8350537055531853577414
Michiel Coulier <michielcoulier@gmail.com>
2020-09-29 14:50:36 +0200

    got name from array working with search

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $pokemonData = file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $_GET['id']);
-$userInput = $_POST['pokeInput'];
-var_dump(json_decode($pokemonData, true));
+$data =  json_decode($pokemonData, true);
 
+echo $data['name'];
+
+echo count($pokemonData);
