231534ae859f003b4f76d845bbd1aa6e8808f0c5
Michiel Coulier <michielcoulier@gmail.com>
2020-09-30 10:14:49 +0200

    added species api url, encoding json from evolutions

--- index.php
+++ index.php
@@ -5,11 +5,14 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $pokemonData = file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $_GET['id']);
+$speciesData = file_get_contents('https://pokeapi.co/api/v2/pokemon-species/' . $_GET ['name']);
 $data =  json_decode($pokemonData, true);
+$evoData = json_decode($speciesData, true);
 $image = $data['sprites']['front_shiny'];
 $imageData = base64_encode(file_get_contents($image));
 $randArray = [];
 
+echo json_encode($evoData);
 echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
 echo $data['id'];
 echo $data['name'];
