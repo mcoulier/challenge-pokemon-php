be2c940ff6604b9c54637848e461d1116c5808f8
Michiel Coulier <michielcoulier@gmail.com>
2020-09-30 11:39:02 +0200

    need to do string conversion

--- index.php
+++ index.php
@@ -6,8 +6,13 @@ error_reporting(E_ALL);
 
 $pokemonData = file_get_contents('https://pokeapi.co/api/v2/pokemon/' . $_GET['id']);
 $speciesData = file_get_contents('https://pokeapi.co/api/v2/pokemon-species/' . $_GET ['id']);
+
 $data =  json_decode($pokemonData, true);
 $evoData = json_decode($speciesData, true);
+
+$evoChain = file_get_contents ($evoData ["evolution_chain"]["url"]);
+$evoDecode = json_decode($evoChain, true);
+
 $image = $data['sprites']['front_shiny'];
 $imageData = base64_encode(file_get_contents($image));
 $randArray = [];
@@ -16,12 +21,16 @@ echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
 echo $data['id'];
 echo $data['name'];
 
-echo $evoData["evolves_from_species"]["name"];
-
 for ($i = 0; $i < 5; $i++){
     $value = rand(0, count($data['moves']));
     $randArray[] = $value;
     echo $data['moves'][json_encode($value)]['move']['name'];
 }
+
+echo $evoData["evolves_from_species"]["name"];
+echo $evoDecode['chain']['evolves_to'];
+echo $evoDecode["chain"]["evolves_to"][0]["evolves_to"];
+
+
 
 //echo var_dump($data);
